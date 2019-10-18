b75d9b2042895c2df341ae5248b36081a8f0292b
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-10-18 11:27:45 -0500

    Working characters cards and API calls. Using dummy data to avoid lockouts

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,63 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { testdata } from "../testdata";
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const CharList = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+  `;
+  const [characters, setCharacters] = useState();
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    setCharacters(testdata);
   }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://rickandmortyapi.com/api/character/")
+  //     .then(response => {
+  //       const apiPromises = [];
+  //       const apiPages = response.data.info.pages;
 
+  //       //USE PAGES FROM API TO CREATE LIST OF PROMISES
+  //       for(let i = 1; i <= apiPages; i++){
+  //         apiPromises.push(axios.get(`https://rickandmortyapi.com/api/character/?page=${i}`))
+  //       }
+
+  //       //GET ALL THE CHARACTERS ONTO ONE DATA SET FROM MULTIPLE PROMISES
+  //       Promise.all(apiPromises)
+  //       .then(responses => {
+  //         const apiResponses = [];
+  //         responses.map((response) => {
+  //           // apiResponses.push(response.data.results);
+  //           response.data.results.map((element) => {
+  //             apiResponses.push(element);
+  //           })
+  //         })
+
+  //         //SET CHARACTERS FROM API CALLS
+  //         setCharacters(apiResponses);
+  //       })
+  //     })
+  // }, []);
+
+  if (!characters) {
+    return <>LOADING...</>;
+  }
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <>
+      <h2>Characters</h2>
+      <CharList>
+        {characters.map((character, index) => (
+          <CharacterCard key={index} character={character} />
+        ))}
+      </CharList>
+      </>
   );
 }
---
+++ src/testdata.js
@@ -0,0 +1,288 @@
+export const testdata=[
+    {
+        id: 481,
+        name: "Retired General Rick",
+        status: "unknown",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/481.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/481",
+        created: "2018-05-22T17:29:50.642Z"
+        },
+        {
+        id: 482,
+        name: "Secret Service Rick",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/482.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/482",
+        created: "2018-05-22T17:32:32.561Z"
+        },
+        {
+        id: 483,
+        name: "Steve Jobs Rick",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/483.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/483",
+        created: "2018-05-22T17:33:33.815Z"
+        },
+        {
+        id: 484,
+        name: "Sheik Rick",
+        status: "Dead",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/484.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/484",
+        created: "2018-05-22T17:35:26.250Z"
+        },
+        {
+        id: 485,
+        name: "Modern Rick",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/485.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/485",
+        created: "2018-05-22T17:36:56.925Z"
+        },
+        {
+        id: 486,
+        name: "Tan Rick",
+        status: "Dead",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/486.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/486",
+        created: "2018-05-22T17:37:37.764Z"
+        },
+        {
+        id: 487,
+        name: "Visor Rick",
+        status: "Dead",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/487.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/487",
+        created: "2018-05-22T17:38:28.593Z"
+        },
+        {
+        id: 488,
+        name: "Colonial Rick",
+        status: "Dead",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/488.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/488",
+        created: "2018-05-22T17:39:06.439Z"
+        },
+        {
+        id: 489,
+        name: "P-Coat Rick",
+        status: "Dead",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Citadel of Ricks",
+        url: "https://rickandmortyapi.com/api/location/3"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/489.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/28"
+        ],
+        url: "https://rickandmortyapi.com/api/character/489",
+        created: "2018-05-22T17:39:37.604Z"
+        },
+        {
+        id: 490,
+        name: "Chang",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "The Menagerie",
+        url: "https://rickandmortyapi.com/api/location/25"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/490.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/29"
+        ],
+        url: "https://rickandmortyapi.com/api/character/490",
+        created: "2018-05-22T17:43:52.543Z"
+        },
+        {
+        id: 491,
+        name: "Dr. Eleanor Arroway",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Female",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "The Menagerie",
+        url: "https://rickandmortyapi.com/api/location/25"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/491.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/29"
+        ],
+        url: "https://rickandmortyapi.com/api/character/491",
+        created: "2018-05-22T17:44:32.071Z"
+        },
+        {
+        id: 492,
+        name: "Varrix",
+        status: "Alive",
+        species: "Alien",
+        type: "",
+        gender: "unknown",
+        origin: {
+        name: "unknown",
+        url: ""
+        },
+        location: {
+        name: "Earth (Replacement Dimension)",
+        url: "https://rickandmortyapi.com/api/location/20"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/492.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/30"
+        ],
+        url: "https://rickandmortyapi.com/api/character/492",
+        created: "2018-05-22T17:49:13.883Z"
+        },
+        {
+        id: 493,
+        name: "Secretary of the Interior",
+        status: "Alive",
+        species: "Human",
+        type: "",
+        gender: "Male",
+        origin: {
+        name: "Earth (Replacement Dimension)",
+        url: "https://rickandmortyapi.com/api/location/20"
+        },
+        location: {
+        name: "Earth (Replacement Dimension)",
+        url: "https://rickandmortyapi.com/api/location/20"
+        },
+        image: "https://rickandmortyapi.com/api/character/avatar/493.jpeg",
+        episode: [
+        "https://rickandmortyapi.com/api/episode/31"
+        ],
+        url: "https://rickandmortyapi.com/api/character/493",
+        created: "2018-05-22T17:51:07.373Z"
+        }
+    ]
